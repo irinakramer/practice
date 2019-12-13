e18725b4324a37d3a79155cbd189b48bc75870d2
Irina Kramer <irakramer@gmail.com>
2019-12-12 22:42:09 -0800

    Multiple pointers - count unique values

---
+++ countUniqueValues.js
@@ -0,0 +1,24 @@
+/**
+MULTIPLE POINTERS - COUNT UNIQUE VALUES
+Implement a function called countUniqueValues, which accepts a sorted array, 
+and counts the unique values in the array. There can be negative numbers 
+in the array, but it will always be sorted.
+*/
+
+ function countUniqueValues(arr) {
+     if (arr.length === 0) {return 0}
+     let i = 0;
+     for (let j = 1; j < arr.length; j++) {
+         if (arr[i] !== arr[j]) {
+             i++;
+             arr[i] = arr[j];            
+         }
+         //console.log(i, j);
+     }
+     return i + 1;
+ }
+
+countUniqueValues([1,1,1,1,2]) // 2
+countUniqueValues([1,2,3,4,4,4,7,7,12,12,13]) // 7
+countUniqueValues([]) // 0
+countUniqueValues([-2,-1,-1,0,1]) // 4
