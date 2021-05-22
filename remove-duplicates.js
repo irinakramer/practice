c2db1e5865f4c2753fac38d7cb78b5c99d89fff6
Irina Kramer <irakramer@gmail.com>
2021-05-22 12:11:34 -0700

    Added reduce method to remove-duplicates

--- remove-duplicates.js
+++ remove-duplicates.js
@@ -1,5 +1,6 @@
 const str = 'This is is a test test string test';
 
+// Method 1 - using Set
 function removeDuplicates(str) {
     const arr = str.split(' '); // create array from string
     const uniqueSet = new Set(arr); // new Set, only stores unique values
@@ -8,3 +9,20 @@ function removeDuplicates(str) {
 }
 
 console.log(removeDuplicates(str));
+
+// Method 2 - using array.reduce()
+function removeDuplicatesReduce(str) {
+    const arr = str.split(' ');  // create array from string
+
+    // reduce arr to include only unique values
+    const uniqueArr = arr.reduce((acc, curr) => {
+        if(acc.indexOf(curr) === -1) {
+            acc.push(curr)
+        }
+        return acc;
+    }, [])
+    // convert arr back into string
+    return uniqueArr.join(' ');
+}
+
+console.log(removeDuplicatesReduce(str));
