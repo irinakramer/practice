8e5131d1d15dbe4ce45b3330dce4f16bfbdcb5b9
Irina Kramer <irakramer@gmail.com>
2021-05-23 12:44:28 -0700

    Added debounce and throttle

---
+++ debounce.js
@@ -0,0 +1,60 @@
+/* DEBOUNCE */
+
+// functionToBeExecuted(); - ignore
+// functionToBeExecuted(); - ignore
+// functionToBeExecuted(); - ignore
+// functionToBeExecuted(); - ignore
+// functionToBeExecuted(); - run this one after timeout
+
+function debounce(fn, time) {
+    let setTimeoutId;
+
+    return function() {
+        if(setTimeoutId) {
+            clearTimeout(setTimeoutId);
+        }
+        setTimeoutId = setTimeout( () => {
+            fn.apply(this, arguments);
+            setTimeoutId = null;
+        }, time)
+    }
+}
+
+/* THROTTLE */
+
+// functionToBeExecuted(); - run this one 
+// functionToBeExecuted(); - ignore
+// functionToBeExecuted(); - ignore
+// functionToBeExecuted(); - ignore
+// functionToBeExecuted(); - run this one after timeout
+function throttle(fn, time) {
+    let setTimeoutId;
+
+    return function() {
+        if(setTimeoutId) {
+            return;
+        }
+        setTimeoutId = setTimeout( () => {
+            fn.apply(this, arguments);
+            setTimeoutId = null;
+        }, time)
+    }
+}
+
+
+
+// https://levelup.gitconnected.com/debounce-in-javascript-improve-your-applications-performance-5b01855e086
+// const debounce = (func, wait) => {
+//     let timeout;
+
+//     return function executedFunction(...args) {
+//         const later = () => {
+//             clearTimeout(timeout);
+//             func(...args);
+//         }
+//         clearTimeout(timeout);
+//         timeout = setTimeout(later, wait)
+//     }
+// }
+
+ 
