76af764a00c07cd4c01a6cff29ec87f80adb267a
Irina Kramer <irakramer@gmail.com>
2021-05-24 13:32:07 -0700

    Added comments

--- debounce.js
+++ debounce.js
@@ -10,10 +10,13 @@ function debounce(fn, time) {
     let setTimeoutId;
 
     return function() {
+        // check for existing calls
         if(setTimeoutId) {
+            // reset timer
             clearTimeout(setTimeoutId);
         }
         setTimeoutId = setTimeout( () => {
+            // invoke fn
             fn.apply(this, arguments);
             setTimeoutId = null;
         }, time)
@@ -44,17 +47,17 @@ function throttle(fn, time) {
 
 
 // https://levelup.gitconnected.com/debounce-in-javascript-improve-your-applications-performance-5b01855e086
-// const debounce = (func, wait) => {
-//     let timeout;
+const debounce = (func, wait) => {
+    let timeout;
 
-//     return function executedFunction(...args) {
-//         const later = () => {
-//             clearTimeout(timeout);
-//             func(...args);
-//         }
-//         clearTimeout(timeout);
-//         timeout = setTimeout(later, wait)
-//     }
-// }
+    return function executedFunction(...args) {
+        const later = () => {
+            clearTimeout(timeout);
+            func(...args);
+        }
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait)
+    }
+}
 
  
