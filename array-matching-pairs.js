db703f53a17a7eb2c91e2ffe6df6ebe385e61603
Irina Kramer <irakramer@gmail.com>
2021-05-25 14:15:23 -0700

    Added Array matching pairs

---
+++ array-matching-pairs.js
@@ -0,0 +1,24 @@
+const array = [10, 20, 20, 10, 10, 30, 50, 10, 20];
+n = array.length;
+
+function sockMerchant(n, ar) {
+    // sort the array
+    ar.sort();
+    //create object with elements as keys, and frequencies as values
+    const mapped = ar.reduce((map, val) => {
+        map[val] = (map[val] || 0) + 1;
+        return map;
+    }, {});
+    // put frequencies into an array
+    const values = Object.values(mapped);
+    // get pairs from the frequencies
+    const pairs = values.reduce((acc, curr) => {
+        if (curr > 1) {
+            acc += Math.floor(curr / 2)
+        }
+        return acc;
+    }, 0)
+    return pairs;
+}
+
+sockMerchant(n, array);
