6d6e0e23cfba14362ceb3c5ad0333ec994a4f0c4
Irina Kramer <irakramer@gmail.com>
2021-06-08 00:02:50 -0700

    added search for property in json

---
+++ fundametal-ES6/006-target-value-in-json.js
@@ -0,0 +1,28 @@
+/**
+ * 6. Write a JavaScript program to target a given value in a nested JSON object, based on the given key.
+ */
+
+const dig = (obj, target) =>
+    target in obj
+        ? obj[target]
+        : Object.values(obj).reduce((acc, curr) => {
+            if (acc !== undefined) return acc;
+            if (typeof curr === 'object') return dig(curr, target)
+        }, undefined);
+
+const data = {
+    level1: {
+        level2: {
+            level3: 'some data'
+        }
+    }
+};
+
+const dog = {
+    "status": "success",
+    "message": "https://images.dog.ceo/breeds/african/n02116738_1105.jpg"
+}
+
+console.log(dig(data, 'level3'));
+console.log(dig(data, 'level4'));
+console.log(dig(dog, 'message'));
