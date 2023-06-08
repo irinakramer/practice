0c799586336bbd6a9a617b9d4ed4a1e42cb20db9
Irina Kramer <irakramer@gmail.com>
2023-06-07 18:57:24 -0700

    added Merge Sorted Arrays

---
+++ leetcode/88.MergeSortedArray.js
@@ -0,0 +1,21 @@
+// 88. Merge Sorted Array
+//https://leetcode.com/problems/merge-sorted-array/
+
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+
+// Method 1 - concat both, then sort
+var merge = function (nums1, m, nums2, n) {
+  for (let i = 0; i < n; i++) {
+    nums1[m + i] = nums2[i];
+  }
+
+  nums1.sort((a, b) => a - b);
+};
+
+// TODO - two pointers two inputs
