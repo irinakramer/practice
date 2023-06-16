6a3c15eb89fe5165571e16fd51d0778a397a5ea0
Irina Kramer <irakramer@gmail.com>
2023-06-15 23:39:06 -0700

    added Longest common prefix

---
+++ leetcode/14.LongestCommonPrefix.js
@@ -0,0 +1,42 @@
+// 14. Longest Common Prefix
+
+// https://leetcode.com/problems/longest-common-prefix
+
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+
+var longestCommonPrefix = function (strs) {
+  let longest = strs[0];
+
+  for (let i = 1; i < strs.length; i++) {
+    for (let j = 0; j < longest.length; j++) {
+      if (strs[i][j] !== longest[j]) {
+        longest = longest.substring(0, j);
+      }
+    }
+  }
+
+  return longest;
+};
+
+/**
+EXPLANATION:
+
+strs = ["flower","flow","flight"]
+
+longest = "flower" (first string in array)
+iterate over strs
+
+"flower","flow"
+compare each char in longest and in next string
+if a char is not the same, then truncate the string at this char:
+longest = flow
+
+"flow","flight"
+conpare each char ....
+longest = fl
+ 
+return longest
+*/
