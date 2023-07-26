14f9acfd7608cdbbbd407502005df65fa23d5150
Irina Kramer <irakramer@gmail.com>
2023-07-25 23:23:47 -0700

    Redid valid parenths

--- leetcode/20.ValidParentheses.js
+++ leetcode/20.ValidParentheses.js
@@ -8,50 +8,33 @@
  */
 
 /**
- Input: s = "([]){}"
- ([]){}
- o  c
-([)] --> TODO this case
-o c
+Create stack
+iteraate over s, if open brace matches (, [, { 
+    add a corresponding closing bracket to the stack
+if we encounter a closing bracket we pop the top from the stack and 
+compare it to char
+if they dont' match we return false
   */
 
 var isValid = function (s) {
-  const end = s.length - 1;
-  let open = 0;
-  let close = 1;
-  let farClose = 1;
-  let counter = 0;
+  // if odd number
+  if (s.length % 2 !== 0) return false;
 
-  // it doesn't start with opening parent and doesn't end with closing parent
-  if (s[0] == ')' || s[0] == ']' || s[0] == '}') return false;
-  if (s[end] == '(' || s[end] == '[' || s[end] == '{') return false;
+  let stack = [];
 
-  function check(o, c) {
-    while (o < c) {
-      if (
-        (o === '(' && c === ')') ||
-        (o === '[' && c === ']') ||
-        (o === '{' && c === '}')
-      ) {
-        return true;
-      } else {
-        return false;
-      }
+  for (let char of s) {
+    if (char === '(') {
+      stack.push(')');
+    } else if (char === '[') {
+      stack.push(']');
+    } else if (char === '{') {
+      stack.push('}');
+    } else if (stack.length === 0 || stack.pop() !== char) {
+      return false;
     }
   }
 
-  while (close < s.length) {
-    if (check(s[open], s[close])) {
-      counter += 2;
-      farClose = close;
-    } else {
-      close++;
-    }
-    open = farClose + 1;
-    close = open + 1;
-  }
-
-  return counter === s.length;
+  return stack.length === 0;
 };
 
 console.log(isValid('()')); // true
