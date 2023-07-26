edf102d93cd0eedd28e886e628e9a8a1f7638d0a
Irina Kramer <irakramer@gmail.com>
2023-07-26 09:59:13 -0700

    added orig solution to longest substr

--- leetcode/3.LongestSubstringWithoutRepeatingCharacters.js
+++ leetcode/3.LongestSubstringWithoutRepeatingCharacters.js
@@ -7,9 +7,42 @@
  * @param {string} s
  * @return {number}
  */
-/* 
 
-  */
+// Orig solution
+var lengthOfLongestSubstring = function (s) {
+  // Create longest Map to keep track of unique chars and their indices
+  let longest = new Map();
+  let max = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (!longest.has(s[i])) {
+      // If char is not in longest, then add it
+      longest.set(s[i], i);
+    } else {
+      /**
+    If char is in longest, delete it and all chars before it.
+    To do so:
+    1 - convert longest to array
+    2 - filter out elements with index greater than current char
+    3 - convert filtered arr back to longest Map
+     */
+
+      const arr = Array.from(longest);
+      const filtered = arr.filter((el) => {
+        return el[1] >= longest.get(s[i]);
+      });
+
+      longest = new Map(filtered);
+
+      // Then add back current char
+      longest.set(s[i], i);
+    }
+    // Check if longest size is greater than max
+    if (longest.size > max) max = longest.size;
+  }
+
+  return max;
+};
 
 // sunny's solution
 function lengthOfLongestSubstring(str) {
