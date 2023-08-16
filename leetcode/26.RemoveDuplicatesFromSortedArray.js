348861512bc6491019fafe9d95b157ebacbfdfda
Irina Kramer <irakramer@gmail.com>
2023-08-15 18:00:03 -0700

    Added optimal solution to #26

--- leetcode/26.RemoveDuplicatesFromSortedArray.js
+++ leetcode/26.RemoveDuplicatesFromSortedArray.js
@@ -30,4 +30,49 @@ var removeDuplicates = function (nums) {
   return nums.length;
 };
 
-// TODO - another more optimal solution
+// Method 2: shift elements
+// O(n^2) time | O(1) space
+var removeDuplicates2 = function (nums) {
+  function shift(i) {
+    for (let index = i + 1; index < nums.length; index++) {
+      nums[index - 1] = nums[index];
+    }
+  }
+
+  let i = 1;
+  while (i < nums.length) {
+    if (nums[i - 1] === nums[i]) {
+      shift(i);
+      nums.pop();
+    } else {
+      i++;
+    }
+  }
+
+  return nums.length;
+};
+
+console.log(removeDuplicates2([1, 1, 2])); // 2
+console.log(removeDuplicates2([0, 0, 1, 1, 1, 2, 2, 3, 3, 3, 4])); // 5
+
+// Method 3 - Optimal solution
+// O(n) time | O(1) space
+var removeDuplicates3 = function (nums) {
+  let left = 1,
+    right = 1;
+
+  while (right < nums.length) {
+    // compare prev of left to right
+    if (nums[left - 1] === nums[right]) {
+      right++;
+    } else {
+      nums[left] = nums[right];
+      right++;
+      left++;
+    }
+  }
+
+  return left;
+};
+
+console.log(removeDuplicates3([0, 0, 1, 1, 1, 2, 2, 3, 3, 3, 4])); // 5
