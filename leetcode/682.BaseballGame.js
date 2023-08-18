8b75b291dc997cbac12d86497b161f6b1a314993
Irina Kramer <irakramer@gmail.com>
2023-08-17 18:11:05 -0700

    Improved Baseball Game

--- leetcode/682.BaseballGame.js
+++ leetcode/682.BaseballGame.js
@@ -27,27 +27,32 @@
 // Time O(n) | Space O(n)
 var calPoints = function (operations) {
     let record = [];
+    let sum = 0;
 
     for (let el of operations) {
         if (el === 'C') {
+            sum -= record[record.length - 1];
             record.pop();
         } else if (el === 'D') {
             // double the last one
             let result = record[record.length - 1] * 2;
             // push it into arr
             record.push(result);
+            sum += result;
         } else if (el === '+') {
             // sum of two last ones
             let result =
                 record[record.length - 1] + record[record.length - 2];
             // add it into arr
             record.push(result);
+            sum += result;
         } else {
             record.push(+el);
+            sum += +el;
         }
     }
 
-    return record.reduce((acc, curr) => acc + curr, 0);
+    return sum;
 };
 
 console.log(calPoints(['5', '2', 'C', 'D', '+'])); // 30
