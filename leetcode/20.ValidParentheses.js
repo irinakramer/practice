fe14140427a13a383d7d3a779e136ad97bbe4d62
Irina Kramer <irakramer@gmail.com>
2023-08-17 19:13:00 -0700

    Added better method to valid parenthesis

--- leetcode/20.ValidParentheses.js
+++ leetcode/20.ValidParentheses.js
@@ -8,6 +8,7 @@
  */
 
 /**
+METHOD 1
 Create stack
 iteraate over s, if open brace matches (, [, { 
     add a corresponding closing bracket to the stack
@@ -17,26 +18,56 @@ if they dont' match we return false
   */
 
 var isValid = function (s) {
-  // if odd number
-  if (s.length % 2 !== 0) return false;
+    // if odd number
+    if (s.length % 2 !== 0) return false;
 
-  let stack = [];
+    let stack = [];
 
-  for (let char of s) {
-    if (char === '(') {
-      stack.push(')');
-    } else if (char === '[') {
-      stack.push(']');
-    } else if (char === '{') {
-      stack.push('}');
-    } else if (stack.length === 0 || stack.pop() !== char) {
-      return false;
+    for (let char of s) {
+        if (char === '(') {
+            stack.push(')');
+        } else if (char === '[') {
+            stack.push(']');
+        } else if (char === '{') {
+            stack.push('}');
+        } else if (stack.length === 0 || stack.pop() !== char) {
+            return false;
+        }
     }
-  }
 
-  return stack.length === 0;
+    return stack.length === 0;
 };
 
 console.log(isValid('()')); // true
 console.log(isValid('()[]{}')); // true
 console.log(isValid('(]')); // false
+
+/**
+METHOD 2 - better performance
+ Idea to push opening brackets into stack, then pop them if matching
+ closing is found.
+
+Iterate over string
+if char is closing and stack is not empty
+and if top of stack matches closing char, we pop it from stack
+if it's not matching, means it's not there we return false
+if it's an opening char and we add to stack
+  */
+
+// Time O(n) | Space O(n)
+var isValid = function (s) {
+    const stack = [];
+    const charMap = { ')': '(', ']': '[', '}': '{' };
+
+    for (let char of s) {
+        if (char in charMap && stack.length) {
+            if (charMap[char] === stack[stack.length - 1])
+                stack.pop();
+            else return false;
+        } else {
+            stack.push(char);
+        }
+    }
+
+    return stack.length === 0;
+};
