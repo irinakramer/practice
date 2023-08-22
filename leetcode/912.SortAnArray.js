ef93cb385337ea8242e55502468d6fb4d34c7d16
Irina Kramer <irakramer@gmail.com>
2023-08-22 15:38:30 -0700

    Added merge sort to sort array #912

--- leetcode/912.SortAnArray.js
+++ leetcode/912.SortAnArray.js
@@ -40,4 +40,72 @@ var sortArray = function (nums) {
 
 console.log(sortArray([5, 2, 3, 1])); // [ 1, 2, 3, 5 ]
 
-// TODO - solve with O(nlog(n))
+// Method 2 - Merge sort
+// Time O(nlog(n)) | Space (O(n))
+var sortArray = function (nums) {
+    function merge(arr, l, m, r) {
+        // Find lengths of two subarrays to be merged
+        let length1 = m - l + 1;
+        let length2 = r - m;
+
+        // Create temp subarrays
+        let left = new Array(length1);
+        let right = new Array(length2);
+
+        // Copy the sorted left & right halfs to temp arrays
+        for (let i = 0; i < length1; i++) {
+            left[i] = arr[l + i];
+        }
+
+        for (let j = 0; j < length2; j++) {
+            right[j] = arr[m + 1 + j];
+        }
+
+        // initial indexes of left and right sub-arrays
+        let i = 0; // index for left
+        let j = 0; // index for right
+        let k = l; // Initial index of merged subarray array
+
+        // Merge the two sorted halfs into the original array
+        while (i < length1 && j < length2) {
+            if (left[i] <= right[j]) {
+                arr[k] = left[i];
+                i++;
+            } else {
+                arr[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+
+        // One of the halfs will have elements remaining
+        while (i < length1) {
+            arr[k] = left[i];
+            i++;
+            k++;
+        }
+        while (j < length2) {
+            arr[k] = right[j];
+            j++;
+            k++;
+        }
+    }
+
+    function mergeSort(arr, l, r) {
+        if (l == r) return arr;
+        // divide in 2
+        let m = Math.floor((l + r) / 2);
+
+        // mergesort 1
+        mergeSort(arr, l, m);
+        // mergesort 2
+        mergeSort(arr, m + 1, r);
+        // merge 1 and 2
+        merge(arr, l, m, r);
+        return arr;
+    }
+
+    return mergeSort(nums, 0, nums.length - 1);
+};
+
+console.log(sortArray([5, 1, 1, 2, 0, 0])); // [ 0, 0, 1, 1, 2, 5 ]
