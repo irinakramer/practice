8aa3e2d899ea81a983d75953323da3e862c0eac1
Irina Kramer <irakramer@gmail.com>
2023-08-24 12:02:26 -0700

    Updates to twoSum

--- leetcode/1.TwoSum.js
+++ leetcode/1.TwoSum.js
@@ -1,36 +1,44 @@
 // 1. Two Sum
+// Easy
+
 // https://leetcode.com/problems/two-sum/description/
 
 /* 
-Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
-You can return the answer in any order.
-
 Input: nums = [2,7,11,15], target = 9
 Output: [0,1]
 Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 */
 
-const nums = [2, 7, 11, 15]
-const target = 9
+/**
+ * Method 1 - using hash map
+ * Time O(n) || Space O(n)
+ * 
+1,3,4,2     - 6
+      ^
+x = 6 - el 
+x = 6 - 1 = 5
+x = 6 - 3 = 3
+x = 6 - 4 = 2 
+x = 6 - 2 = 4 yes ==> [2, 3] // 2 is index of 4, and 3 is index of 2
 
-const twoSum = (nums, target) => {
-   const myMap = new Map();
+{
+1:0
+3:1
+4:2
+}
+*/
+
+var twoSum = function (nums, target) {
+    let map = new Map();
 
     for (let i = 0; i < nums.length; i++) {
-      let curr = nums[i];
-      let x = target - curr;
-
-      if(myMap.has(x)) {
-        return [myMap.get(x), i]
-      }
-
-      myMap.set(curr, i)
+        if (!map.has(target - nums[i])) {
+            map.set(nums[i], i);
+        } else {
+            return [map.get(target - nums[i]), i];
+        }
     }
+};
 
-    return null;
-}
-
-twoSum(nums, target)
+console.log(twoSum([2, 7, 11, 15], 9)); // [ 0, 1 ]
+console.log(twoSum([1, 3, 4, 2], 6)); // [ 2, 3 ]
