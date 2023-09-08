bbbdb3ac006155ab82466114fbc32cf9e60ea237
Irina Kramer <irakramer@gmail.com>
2023-09-07 18:01:14 -0700

    Added new solution to #80 - remove dups

--- leetcode/80.RemoveDuplicatesFromSortedArrayII.js
+++ leetcode/80.RemoveDuplicatesFromSortedArrayII.js
@@ -9,29 +9,29 @@
  */
 // Method 1 - Solution with Map, O(n) time, O(n) space
 var removeDuplicates = function (nums) {
-  let map = new Map();
-  let temp = [];
+    let map = new Map();
+    let temp = [];
 
-  let i = 0;
-  while (i < nums.length) {
-    if (!map.has(nums[i])) {
-      map.set(nums[i], 1);
-      temp.push(nums[i]);
-      i++;
-    } else if (map.get(nums[i]) === 1) {
-      map.set(nums[i], 2);
-      temp.push(nums[i]);
-      i++;
-    } else {
-      i++;
+    let i = 0;
+    while (i < nums.length) {
+        if (!map.has(nums[i])) {
+            map.set(nums[i], 1);
+            temp.push(nums[i]);
+            i++;
+        } else if (map.get(nums[i]) === 1) {
+            map.set(nums[i], 2);
+            temp.push(nums[i]);
+            i++;
+        } else {
+            i++;
+        }
     }
-  }
 
-  nums.splice(temp.length);
+    nums.splice(temp.length);
 
-  for (let i = 0; i < temp.length; i++) {
-    nums[i] = temp[i];
-  }
+    for (let i = 0; i < temp.length; i++) {
+        nums[i] = temp[i];
+    }
 };
 
 // Method 2 - Modifying array in place, O(n) time, O(1) space
@@ -50,24 +50,66 @@ answer:
   */
 
 var removeDuplicates = function (nums) {
-  let count = 1;
-  let curr = 0;
-  let next = 1;
+    let count = 1;
+    let curr = 0;
+    let next = 1;
 
-  while (next < nums.length) {
-    if (nums[curr] === nums[next]) {
-      if (count < 2) {
-        count++;
-        curr++;
-        next++;
-      } else {
-        nums.splice(next, 1);
-        next = curr + 1;
-      }
-    } else {
-      count = 1;
-      curr++;
-      next++;
+    while (next < nums.length) {
+        if (nums[curr] === nums[next]) {
+            if (count < 2) {
+                count++;
+                curr++;
+                next++;
+            } else {
+                nums.splice(next, 1);
+                next = curr + 1;
+            }
+        } else {
+            count = 1;
+            curr++;
+            next++;
+        }
     }
-  }
+};
+
+/**
+ * Method 3 - 
+ Based on solution from https://www.youtube.com/watch?v=drbtmYjZQHQ
+ Compare Left - 2 to current element, and if they're not the same, 
+ replace Left with current element, increment Left and move to next
+ element
+ Return Left index
+
+ Time O(n) | Space O(1)
+
+ 0 1 2 3 4 5
+ 1,1,1,2,2,3
+     L
+       el
+
+ 1,1,2,2,2,3
+       L
+         el
+ 
+ 1,1,2,2,3,3
+         L
+           el
+ 
+ 1,1,2,2,3,3
+           L
+             el == out
+  */
+
+var removeDuplicates = function (nums) {
+    let L = 0,
+        R = 0;
+
+    for (let el of nums) {
+        if (L === 0 || L === 1 || nums[L - 2] != el) {
+            nums[L] = el;
+            L++;
+        }
+    }
+
+    return L;
 };
