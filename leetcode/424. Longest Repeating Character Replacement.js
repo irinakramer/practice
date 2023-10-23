a96be2f21b370405960d548481d54a0bc10eebc9
Irina Kramer <irakramer@gmail.com>
2023-10-22 22:06:48 -0700

    Added new solution for #424

--- leetcode/424. Longest Repeating Character Replacement.js
+++ leetcode/424. Longest Repeating Character Replacement.js
@@ -19,7 +19,6 @@
  mostF = 
  maxL = 
  
- 
   */
 var characterReplacement = function (s, k) {
     let L = 0,
@@ -42,4 +41,30 @@ var characterReplacement = function (s, k) {
     return res;
 };
 
-// TODD - better time complexity
+// Method 2 - same but using maxF  - better time complexity
+var characterReplacement = function (s, k) {
+    let L = 0,
+        res = 0,
+        maxF = 0;
+    let count = {}; // occurences of each char (A-Z)
+
+    // move thru str by R pointer
+    for (let R = 0; R < s.length; R++) {
+        // increment char of R in the count hashmap
+        count[s[R]] = 1 + (count[s[R]] || 0);
+        maxF = Math.max(maxF, count[s[R]]); // keep track of max frequency
+
+        // if window is invalid, decrement L and move it by 1
+        // invalid window is when number of allowed chars is more than k
+        // R - L + 1 is the length of the current window
+        if (R - L + 1 - maxF > k) {
+            count[s[L]] -= 1;
+            L++;
+        }
+
+        // take max of res or length of curr window
+        res = Math.max(res, R - L + 1);
+    }
+
+    return res;
+};
