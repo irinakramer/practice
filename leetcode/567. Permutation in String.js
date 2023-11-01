bdb5cff2563081ed1e6367179be5ce0e7ed6c70c
Irina Kramer <irakramer@gmail.com>
2023-11-01 15:00:49 -0700

    Added hashmaps solution to #567

--- leetcode/567. Permutation in String.js
+++ leetcode/567. Permutation in String.js
@@ -40,4 +40,66 @@ var checkInclusion = function (s1, s2) {
 console.log(checkInclusion("hello", "ooolleoooleh")); // false
 console.log(checkInclusion("ab", "eidbaooo")); // true
 
-// TODO - better time complexity with hashmap lookup
+// Method 2 - compare hasmaps
+/**
+ab
+map1 = {
+    a: 1
+    b: 1
+}
+eidbaooo
+01234567
+   l
+    r
+map2 = {
+    b: 1
+    a: 1
+}
+build a map from l to r
+
+compare two maps
+if they're the same, return true
+
+otherwise return false
+  */
+var checkInclusion = function (s1, s2) {
+    const newMap = (s) => {
+        let map = new Map();
+        for (let i = 0; i < s.length; i++) {
+            let el = s[i];
+
+            if (!map.get(el)) {
+                map.set(el, 1);
+            } else {
+                map.set(el, map.get(el) + 1);
+            }
+        }
+        return map;
+    };
+
+    const compareMaps = (a, b) => {
+        if (a.size !== b.size) return false;
+
+        for (let [key, val] of a) {
+            let bVal = b.get(key);
+            if (bVal !== val || !b.has(key)) {
+                return false;
+            }
+        }
+
+        return true;
+    };
+
+    const map1 = newMap(s1);
+
+    for (let l = 0, r = s1.length - 1; r < s2.length; l++, r++) {
+        let sub = s2.substring(l, r + 1);
+        const map2 = newMap(sub);
+
+        if (compareMaps(map1, map2)) {
+            return true;
+        }
+    }
+
+    return false;
+};
