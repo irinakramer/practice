c1e003d0110211bd946a7d8a718136b72dcfb846
Irina Kramer <irakramer@gmail.com>
2024-06-08 13:38:11 -0700

    Added two pointers solution to 88

--- leetcode/88.MergeSortedArray.js
+++ leetcode/88.MergeSortedArray.js
@@ -11,11 +11,28 @@
 
 // Method 1 - concat both, then sort
 var merge = function (nums1, m, nums2, n) {
-  for (let i = 0; i < n; i++) {
-    nums1[m + i] = nums2[i];
-  }
+    for (let i = 0; i < n; i++) {
+        nums1[m + i] = nums2[i];
+    }
 
-  nums1.sort((a, b) => a - b);
+    nums1.sort((a, b) => a - b);
 };
 
-// TODO - two pointers two inputs
+// Solution 2 - Two pointers and iterate from the end of arrays
+// Time: O(m+n)
+// Space: 0(1)
+let i = m - 1;
+let j = n - 1;
+let k = m + n - 1;
+
+while (j >= 0) {
+    if (i >= 0 && nums1[i] > nums2[j]) {
+        nums1[k] = nums1[i];
+        k--;
+        i--;
+    } else {
+        nums1[k] = nums2[j];
+        k--;
+        j--;
+    }
+}
